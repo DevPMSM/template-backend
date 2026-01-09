23eae5b3f450168191a84795ed657e2e42c7a4b8
LucasELopes <lucaslopeslll412@gmail.com>
2026-01-09 09:56:03 -0300

    refactor: template update

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,21 +3,22 @@
 namespace App\Http\Controllers;
 /**
  * @OA\Info(
- *      title="API VACINES",
- *      version="1.0.0",
- *      description=" API SISTEMAS VACINAÇÃO
- *      Documentação da API para facilitar acessos e testes no Sistema de Vacinas
- *      Feito pelo setor de TI da Prefeitura de São Mateus.
- *      PARA UTILIZAR AS ROTAS COMECE FAZENDO LOGIN",
- * ),
+ *     title="(INSIRA NOME DA API)",
+ *     version="1.0.0",
+ *     description="Documentação da API para facilitar acessos e testes no (INSIRA O NOME DA API).
+ * Feito pelo setor de TI da Prefeitura de São Mateus.
+ * PARA UTILIZAR AS ROTAS, COMECE FAZENDO LOGIN."
+ * )
+ *
  * @OA\Tag(
- *      name="Login/Logout",
- *      description="Operações de autenticação",
- *  ),
+ *     name="Login/Logout",
+ *     description="Operações de autenticação"
+ * )
+ *
  * @OA\Tag(
- *       name="Usuários",
- *       description="Operações com usuários (ADMIN)",
- *  )
+ *     name="Usuários",
+ *     description="Operações com usuários (ADMIN)"
+ * )
  */
 abstract class Controller
 {
--- routes/api.php
+++ routes/api.php
@@ -1,27 +1,8 @@
 <?php
 
-use App\Http\Controllers\Auth\AuthenticatedTokenController;
-use App\Http\Controllers\ExampleCRUDController;
-use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login', [AuthenticatedTokenController::class, 'store']);
-Route::post('/logout', [AuthenticatedTokenController::class, 'destroy'])->middleware('auth:sanctum');
-
-Route::middleware(['auth:sanctum'])->group(function() {
-    Route::prefix('/users')->controller(UserController::class)->group(function () {
-        Route::get('/{user}', 'show');
-        Route::put('/{user}', 'update');
-    });
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
 });
-
-Route::middleware(['auth:sanctum', 'can:admin'])->group(function(){
-    Route::apiResource('/users',UserController::class)->except(['update', 'show']);
-    Route::delete('/users/hard-delete/{user}', [UserController::class, 'hardDelete']);
-});
-
-// Rotas do "EXAMPLECRUD", apenas para fins de teste.
-// Route::middleware(['auth:sanctum'])->group(function(){
-//     Route::apiResource('/exampleCRUD', ExampleCRUDController::class);
-//     Route::delete('/exampleCRUD/hard-delete/{exampleCRUD}', [ExampleCRUDController::class, 'hardDelete']);
-// });
