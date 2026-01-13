6048ec9efec7f2d9725334d759acdb22365c6220
LucasELopes <lucaslopeslll412@gmail.com>
2026-01-13 14:44:49 -0300

    feat: add users endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,22 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthenticatedTokenController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+//Rotas de autenticações
 Route::controller(AuthenticatedTokenController::class)->group(function () {
     Route::post('/login', 'store');
     Route::post('/logout', 'destroy')->middleware('auth:sanctum');
 });
 
-Route::middleware(['auth:sanctum'])->get('/profile', function (Request $request) {
-    return $request->user();
+//Rotas autenticadas
+Route::middleware(['auth:sanctum'])->group(function() {
+    Route::get('/profile', function () {
+        return response()->json(auth()->user());
+    });
+
+    Route::apiResource('/users', UserController::class);
+    Route::delete('/users/{user}/hard-delete', [UserController::class, 'hardDelete']);
 });
