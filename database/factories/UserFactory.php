d07ce7abd9c697d542aa94b3fb3b3f25e32b1065
LucasELopes <lucaslopeslll412@gmail.com>
2026-08-20 15:49:34 -0300

    refactor: add image userFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,6 +29,7 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'contact' => '(99) 99999-9999',
+            'image' => 'https://picsum.photos/440/220?random=' . fake()->unique()->numberBetween(1, 1000),
             'role' => $this->faker->randomElement(User::getRoles()),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
